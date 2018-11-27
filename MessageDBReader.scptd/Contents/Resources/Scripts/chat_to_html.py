65ee3fc3740585767010174010e80e6421740f7a
Kristoffer Anger <kristoffer.anger@gmail.com>
2018-11-27 19:00:33 +0100

    Fixed date bug and added parse error handling.

--- MessageDBReader.scptd/Contents/Resources/Scripts/chat_to_html.py
+++ MessageDBReader.scptd/Contents/Resources/Scripts/chat_to_html.py
@@ -12,37 +12,56 @@ def try_index(array, index, default = None):
     try: return array[index]
     except: return default
 
-def div_class(message):
+def json_from_str(string):
+    try:
+        return ast.literal_eval(string)
+    except ValueError as ex:
+        _exc_type, exc_value, exc_traceback = sys.exc_info()
+        error_text = "ERROR: %r: %s" % (exc_value, string)
+        last_tb = exc_traceback
+        while last_tb.tb_next:
+            last_tb = last_tb.tb_next
+        error_text += "error location: line=%d, col=%d" % (last_tb.tb_frame.f_locals["node"].lineno, last_tb.tb_frame.f_locals["node"].col_offset)
+        osx_epoch = datetime.date(2001,01,01)
+        unix_epoch = datetime.date(1970,01,01)
+        current_time = (int(datetime.datetime.now().strftime('%s')) - (osx_epoch-unix_epoch).total_seconds())*1000000000
+    return [{'text':error_text, 'service':'', 'is_from_me':-1, 'date': current_time}]
 
+def div_class(message):
+    
     from_me = message["is_from_me"]==1
+    from_them = message["is_from_me"]==0
     imessage = message["service"].lower() == "imessage"
     
     if from_me and imessage:
         return "from-me-imessage"
     elif from_me:
         return "from-me"
-    else:
+    elif from_them:
         return "from-them"
+    else:
+        return "error"
     
 def decode_text(message):
     return message.decode('unicode-escape').encode('utf8')
 
-def decode_osx_date(date):
-    #datetime(date + strftime(\'%s\',\'2001-01-01\'), \'unixepoch\') as date_utc \
-    return "2018-11-23, 11:28"
+def decode_osx_date(ts):
     
+    osx_epoch = datetime.date(2001,01,01)
+    unix_epoch = datetime.date(1970,01,01)
 
+    unix_timestamp = ts/1000000000 + (osx_epoch-unix_epoch).total_seconds()
+    date = datetime.datetime.fromtimestamp(unix_timestamp).strftime('%Y-%m-%d %H:%M')
+
+    return date
     
 #define values
-
 html_format = """
 <!DOCTYPE html>
 <html lang="en" class=" -webkit-">
     <head>
         <title>%s</title>
-        <meta charset="UTF-8">
-        <script src="//static.codepen.io/assets/editor/live/console_runner-ce3034e6bde3912cc25f83cccb7caa2b0f976196f2f2d52303a462c826d54a73.js"></script>
-        <script src="//static.codepen.io/assets/editor/live/css_reload-2a5c7ad0fe826f66e054c6020c99c1e1c63210256b6ba07eb41d7a4cb0d0adab.js"></script>        
+        <meta charset="UTF-8">       
         <style media="screen" data-href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css"> html, body, div, span, applet, object, iframe, h1, h2, h3, h4, h5, h6, p, blockquote, pre, a, abbr, acronym, address, big, cite, code, del, dfn, em, img, ins, kbd, q, s, samp, small, strike, strong, sub, sup, tt, var, b, u, i, center, dl, dt, dd, ol, ul, li, fieldset, form, label, legend, table, caption, tbody, tfoot, thead, tr, th, td, article, aside, canvas, details, embed, figure, figcaption, footer, header, hgroup, menu, nav, output, ruby, section, summary, time, mark, audio, video{margin:0; padding:0; border:0; font-size:100%%; font:inherit; vertical-align:baseline}, article, aside, details, figcaption, figure, footer, header, hgroup, menu, nav, section{display: block} body{line-height:1} ol, ul{list-style:none}blockquote, q{quotes:none}blockquote:before, blockquote:after, q:before, q:after{content:''; content:none} table{border-collapse:collapse; border-spacing:0} </style>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"></script>
         <style class="cp-pen-styles">body {
@@ -168,6 +187,14 @@ html_format = """
             margin-left: auto;
             margin-right: auto;
         }
+        .error {
+            position: relative;
+            padding: 10px 20px;
+            background: #E5E5EA;
+            border-radius: 25px;
+            color: black;
+            float: left;
+        }
         </style>
     </head>
     <body>
@@ -178,11 +205,11 @@ html_format = """
 </html>
 """
 
-
-chat = ast.literal_eval(try_index(sys.argv, 1, "[{'text':'Hello world!', 'service':'iMessage', 'is_from_me':1}, {'text':'Hello self!', 'is_from_me':0, 'service':'iMessage'}]"))
+chat_text = str(try_index(sys.argv, 1, "[{'text':'Hello world!', 'service':'iMessage', 'is_from_me':1, 'date': 123456789}, {'text':'Hello self!', 'is_from_me':0, 'service':'iMessage', 'date': 123456789}]"))
 chat_name = str(try_index(sys.argv, 2, 'Unknown'))
 
-#build html string 
+#build html string
+chat = json_from_str(chat_text)
 chat_divs = "\n".join(["<div class=\"date\"><p>%s</p></div><div class=\"%s\"><p>%s</p></div><div class=\"clear\"></div>" % (decode_osx_date(message["date"]), div_class(message), decode_text(message["text"])) for message in chat])
 title = "Chat with %s" %(chat_name)
 result = html_format %(title, chat_divs)
